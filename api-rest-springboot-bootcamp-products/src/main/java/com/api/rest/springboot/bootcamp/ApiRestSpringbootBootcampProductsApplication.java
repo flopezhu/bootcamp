a4007a5f614ca720e9036465f23b9bc30357cb41
flopezhu <franclin.lopezhuayanay@nttdata.com>
2022-05-17 16:05:40 -0500

    add eureka client

--- api-rest-springboot-bootcamp-products/src/main/java/com/api/rest/springboot/bootcamp/ApiRestSpringbootBootcampProductsApplication.java
+++ api-rest-springboot-bootcamp-products/src/main/java/com/api/rest/springboot/bootcamp/ApiRestSpringbootBootcampProductsApplication.java
@@ -2,11 +2,13 @@ package com.api.rest.springboot.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiRestSpringbootBootcampProductsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiRestSpringbootBootcampProductsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestSpringbootBootcampProductsApplication.class, args);
+    }
 }
