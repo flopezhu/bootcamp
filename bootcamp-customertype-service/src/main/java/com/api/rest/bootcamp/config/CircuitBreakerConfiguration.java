5a43a67b405463e3421af4ec0dc9c92be9c23452
flopezhu <franclin.lopezhuayanay@nttdata.com>
2022-05-27 16:57:27 -0500

    error dns block api gateway

--- bootcamp-customertype-service/src/main/java/com/api/rest/bootcamp/config/CircuitBreakerConfiguration.java
+++ bootcamp-customertype-service/src/main/java/com/api/rest/bootcamp/config/CircuitBreakerConfiguration.java
@@ -1,15 +1,8 @@
 package com.api.rest.bootcamp.config;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 
 @Configuration
 public class CircuitBreakerConfiguration {
